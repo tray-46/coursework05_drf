39ec1cf9fdfa9e221f1c3ad9ecf75f073070543d
tray <at-box@mail.ru>
2026-09-14 08:47:03 +0500

    custom User model added

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
 # Create your models here.
+class User(AbstractUser):
+    """
+    Represent a service user
+    """
+
+    email = models.EmailField(unique=True, verbose_name="Почта", help_text="Укажите почту")
+    telegram_username = models.CharField(max_length=32, null=True, blank=True, verbose_name="Имя пользователя телеграм")
+
+    USERNAME_FIELD = "username"
+    REQUIRED_FIELDS = ["email"]
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
