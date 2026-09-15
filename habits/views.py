03793ec1bbe910bf770794696b51a70912d2077a
tray <at-box@mail.ru>
2026-09-15 13:05:40 +0500

    basic CRUD views and urls for habit

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.permissions import IsAuthenticated
+
+from habits.models import Habit
+from habits.serializers import HabitSerializer
+
 
 # Create your views here.
+class HabitCreateAPIView(CreateAPIView):
+    serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated]
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class HabitListAPIView(ListAPIView):
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class HabitRetrieveAPIView(RetrieveAPIView):
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class HabitUpdateAPIView(UpdateAPIView):
+    queryset = Habit.objects.all()
+    serializer_class = HabitSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class HabitDestroyAPIView(DestroyAPIView):
+    queryset = Habit.objects.all()
+    permission_classes = [IsAuthenticated]
+
