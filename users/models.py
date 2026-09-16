173b9bd9c5e83edf6dca5a9419755686454a9664
tray <at-box@mail.ru>
2026-09-16 12:58:02 +0500

    user model change: telegram username changed to chat id

--- users/models.py
+++ users/models.py
@@ -9,7 +9,7 @@ class User(AbstractUser):
     """
 
     email = models.EmailField(unique=True, verbose_name="Почта", help_text="Укажите почту")
-    telegram_username = models.CharField(max_length=32, null=True, blank=True, verbose_name="Имя пользователя телеграм")
+    telegram_chat_id = models.CharField(max_length=32, null=True, blank=True, verbose_name="Chat_id пользователя телеграм")
 
     USERNAME_FIELD = "username"
     REQUIRED_FIELDS = ["email"]
